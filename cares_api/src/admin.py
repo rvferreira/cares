6a9315129f619c82a2384efa7d519a96b0e618f6
Valeska Paroni <vparoni@localhost.localdomain>
2016-11-19 20:03:50 -0200

    models ok

--- cares_api/src/admin.py
+++ cares_api/src/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User, Ticket
+from .models import User, Ticket, Topic, Career, TicketInsideCareer, Enrollment, Sprint
 
 admin.site.register(User)
 admin.site.register(Ticket)
+admin.site.register(Topic)
+admin.site.register(Career)
+admin.site.register(TicketInsideCareer)
+admin.site.register(Enrollment)
+admin.site.register(Sprint)
--- cares_api/src/models.py
+++ cares_api/src/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.utils.timezone import now
 from datetime import timedelta
 from django.db import models
+import uuid
 
 # Create your models here.
 
@@ -24,17 +25,68 @@ class User(models.Model):
 	password = models.CharField(max_length = 30, default="&0k4f")
 	level_of_education = models.CharField(max_length = 1, choices = education_options, default = '3')
 	user_type = models.CharField(max_length = 1, choices = user_type_options)
-	date_of_birth = models.DateField(auto_now=False, auto_now_add=False, default=now())
+	date_of_birth = models.DateField(auto_now=False, auto_now_add=False, default=now)
 
 	def __unicode__(self):
 		return "User: {0}".format(self.name)
+
+
+class Topic(models.Model):
+	name = models.CharField(max_length=100, primary_key=True)
+
+	def __unicode__(self):
+		return "Topic: {0}".format(self.name)
 
 
 class Ticket(models.Model):
 	name = models.CharField(max_length=140)
 	estimated_time = models.DurationField(default=timedelta(hours=8))
 	importance = models.SmallIntegerField(default=50)
-	description = models.CharField(max_length=500)
+	description = models.TextField(max_length=500)
+	topic = models.ForeignKey(
+		'Topic',
+		on_delete = models.CASCADE,
+		null=True,
+		)
 
 	def __unicode__(self):
 		return "Ticket: {0}".format(self.name)
+
+
+class Career(models.Model):
+	visibility_choices = (
+		(0, 'Privada'),
+		(1, 'Publica')
+		)
+	name = models.CharField(max_length=140)
+	public = models.BooleanField(choices = visibility_choices, default=1)
+
+	def __unicode__(self):
+		return "Carreira: {0}".format(self.name)
+
+
+class TicketInsideCareer(models.Model):
+	ticket = models.ForeignKey(
+		'Ticket',
+		on_delete = models.CASCADE,
+		)
+	career = models.ForeignKey(
+		'Career',
+		on_delete = models.CASCADE,
+		)
+
+
+class Enrollment(models.Model):
+	student = models.ForeignKey(
+		'User',
+		on_delete = models.CASCADE
+		)
+	career = models.ForeignKey(
+		'Career',
+		on_delete = models.CASCADE
+		)
+	sprints = models.ManyToManyField('Sprint')
+
+
+class Sprint(models.Model):
+	tickets = models.ManyToManyField('TicketInsideCareer')
