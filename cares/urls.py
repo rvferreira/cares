6b5e237453454d15fa4d0d84bf46e1236a612bea
Raphael Ferreira <raphael.rvf@gmail.com>
2016-11-23 12:28:41 -0200

    rest_framework setup and git unverbose

--- cares/urls.py
+++ cares/urls.py
@@ -21,5 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^demo/', include('app.urls')),
+    url(r'^api_auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'', include('app.urls')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
