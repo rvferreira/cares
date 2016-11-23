ce7b2353dcb622308020cef325436b1ad0aed337
Raphael Ferreira <raphael.rvf@gmail.com>
2016-11-23 12:07:07 -0200

    templates setup

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import loader, RequestContext
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        'page_title': 'Cares Demo'
+    })
+
+    return HttpResponse(template.render(context))
+
