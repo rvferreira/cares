7a54285595194fc3fa51c3d093c27ac6c6f34940
ValeskaParoni <valeskaparoni@gmail.com>
2016-11-23 12:54:21 -0200

    admin models working

--- app/admin.py
+++ app/admin.py
@@ -3,3 +3,11 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import User, Topic, Ticket, Career, TicketInsideCareer, Enrollment, Sprint
+
+admin.site.register(User)
+admin.site.register(Topic)
+admin.site.register(Ticket)
+admin.site.register(Career)
+admin.site.register(TicketInsideCareer)
+admin.site.register(Enrollment)
+admin.site.register(Sprint)
